1bf104c46139ce4a2db9dee3ed5d51b418f307b9
minsuk <palt1230@naver.com>
2021-06-20 16:20:02 +0900

    comment Controller추가

--- src/main/java/myapp/myapp/controller/comment/CommentController.java
+++ src/main/java/myapp/myapp/controller/comment/CommentController.java
@@ -1,9 +1,12 @@
 package myapp.myapp.controller.comment;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import myapp.myapp.config.auth.LoginUser;
 import myapp.myapp.config.auth.dto.SessionUser;
 import myapp.myapp.controller.ApiResponse;
+import myapp.myapp.service.board.dto.request.CreateBoardRequest;
 import myapp.myapp.service.comment.CommentService;
 import myapp.myapp.service.comment.dto.request.CreateCommentRequest;
 import myapp.myapp.service.comment.dto.response.CommentInfoResponse;
@@ -14,9 +17,24 @@ import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RestController
+@Api(tags = {"댓글 API"})
 public class CommentController {
 
     private final CommentService commentService;
 
+    @ApiOperation(value  = "댓글 쓰기 API")
+    @PostMapping("/api/v1/board/comment")
+    public ApiResponse<CommentInfoResponse> addBoardComment(
+            @Valid @RequestBody CreateCommentRequest request, @LoginUser SessionUser user
+    ) {
+        return ApiResponse.of(commentService.createComment(request, user.getEmail()));
+    }
 
+    @ApiOperation(value  = "댓글 삭제 API")
+    @DeleteMapping("/api/v1/board/comment")
+    public ApiResponse<String> deleteBoardComment(
+            @RequestParam Long commentId, @LoginUser SessionUser user) {
+        commentService.deleteComment(commentId, user.getEmail());
+        return ApiResponse.OK;
+    }
 }
--- src/main/java/myapp/myapp/service/comment/CommentService.java
+++ src/main/java/myapp/myapp/service/comment/CommentService.java
@@ -3,6 +3,8 @@ package myapp.myapp.service.comment;
 import lombok.RequiredArgsConstructor;
 import myapp.myapp.domain.comment.Comment;
 import myapp.myapp.domain.comment.CommentRepository;
+import myapp.myapp.domain.member.Member;
+import myapp.myapp.domain.member.MemberRepository;
 import myapp.myapp.service.comment.dto.request.CreateCommentRequest;
 import myapp.myapp.service.comment.dto.response.CommentInfoResponse;
 import org.springframework.stereotype.Service;
@@ -12,17 +14,24 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class CommentService {
 
+    private final MemberRepository memberRepository;
     private final CommentRepository commentRepository;
 
     @Transactional
-    public CommentInfoResponse createComment(CreateCommentRequest request, Long memberId){
-        Comment comment = commentRepository.save(request.toEntity(memberId));
+    public CommentInfoResponse createComment(CreateCommentRequest request, String name){
+        Member member = memberRepository.findByEmail(name)
+                .orElseThrow(() -> new IllegalArgumentException("사용자 없음"));
+
+        Comment comment = commentRepository.save(request.toEntity(member.getId()));
         return CommentInfoResponse.of(comment);
     }
 
     @Transactional
-    public void deleteComment(Long commentId, Long memberId) {
-        Comment comment = CommentServiceUtils.findCommentByIdAndMemberId(commentRepository, commentId, memberId);
+    public void deleteComment(Long commentId, String name) {
+        Member member = memberRepository.findByEmail(name)
+                .orElseThrow(() -> new IllegalArgumentException("사용자 없음"));
+
+        Comment comment = CommentServiceUtils.findCommentByIdAndMemberId(commentRepository, commentId, member.getId());
         comment.delete();
     }
 }
